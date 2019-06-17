fd24568483da6329235b71b6fd71bef6fb25f007
Chris Kay <chriskay25@gmail.com>
2019-06-17 22:04:46 +0000

    Put some placeholder info in Beer class.

--- lib/cheers/beer.rb
+++ lib/cheers/beer.rb
@@ -1,7 +1,21 @@
 class Cheers::Beer 
   
-  def beer 
+  attr_accessor :name, :style, :strength 
+  
+  def all_the_beer
     # gathers and returns a list of beers and their properties
+    
+    beer1 = self.new 
+    beer1.name = "Stouty McStouterson"
+    beer1.style = "Stout"
+    beer1.strength = "5.2%"
+    
+    beer2 = self.new 
+    beer2.name = "Porty McPorterson"
+    beer2.style = "Porter" 
+    beer2.strength = "6.6%"
+    
+    [beer1, beer2]
   end 
   
 end 
