dd15c5e45903a22e8dd910cfd108cb98d131578a
Chris Kay <chriskay25@gmail.com>
2019-06-17 18:59:44 +0000

    CLI placeholder edits

--- lib/cheers/cli.rb
+++ lib/cheers/cli.rb
@@ -8,16 +8,27 @@ class Cheers::CLI
   end 
   
   def greeting 
-    puts "Hello. This CLI includes information about breweries/bars in Downtown Atlanta as well as the beer they sell."
+    puts "Welcome. This CLI includes information about breweries/bars in Downtown Atlanta as well as the beer they sell."
   end 
   
   def options 
-    puts "If you would like to learn more about one of the following options, type its corresponding number and press 'Enter':"
+    puts "To select one of the following options, type its corresponding number and press 'Enter':"
     puts <<-DOC 
-      1. Breweries
-      2. Bars 
-      3. Beer 
+      1. I want a list of Atlanta breweries and bars
+      2. I already have a brewery or bar in mind, show me their beer
+      3. Beer ye, beer ye
     DOC
+    
+    input = nil 
+    while input != "exit" 
+      if input == "1"
+        puts "All the breweries"
+      elsif input == "2"
+        puts "These are the beers sold at this location"
+      elsif input == "3" 
+        puts "Info about beer"
+      end 
+    end 
   end 
   
 end 
