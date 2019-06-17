cbd33ed6eb604ed44e7c0b8327ee1d757baf0fef
Chris Kay <chriskay25@gmail.com>
2019-06-17 18:34:17 +0000

    Added some placeholder methods into the cli controller.

--- lib/cheers/cli.rb
+++ lib/cheers/cli.rb
@@ -3,7 +3,21 @@
 class Cheers::CLI
   
   def call 
-    puts "ATL Breweries" 
+    greeting
+    options 
+  end 
+  
+  def greeting 
+    puts "Hello. This CLI includes information about breweries/bars in Downtown Atlanta as well as the beer they sell."
+  end 
+  
+  def options 
+    puts "If you would like to learn more about one of the following options, type its corresponding number and press 'Enter':"
+    puts <<-DOC 
+      1. Breweries
+      2. Bars 
+      3. Beer 
+    DOC
   end 
   
 end 
