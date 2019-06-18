8e342d31b883734dc3522a4338d55e0aaa2db20a
Chris Kay <chriskay25@gmail.com>
2019-06-18 19:35:29 +0000

    Added nokogiri to the gemspec.

--- lib/cheers/scraper.rb
+++ lib/cheers/scraper.rb
@@ -1,7 +1,8 @@
 class Cheers::Scraper
   
   def breweries_page 
-    breweries_page = Nokogiri::HTML(open("https://www.beeradvocate.com/place/city/1/")
+    breweries_page = Nokogiri::HTML(open("https://www.beeradvocate.com/place/city/1/"))
+    breweries_page.css("div#ba-content ul li[1] a")
   end 
   
 end 
