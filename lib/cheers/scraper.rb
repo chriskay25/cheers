5f05230a3da51c080898fbf10014848b37d7eed2
chriskay <chriskay25@gmail.com>
2019-06-19 12:01:06 -0400

    Edited scraper class to try to separate concerns.

--- lib/cheers/scraper.rb
+++ lib/cheers/scraper.rb
@@ -1,12 +1,20 @@
 class Cheers::Scraper
   
-  def breweries
-    # Stores the HTML in the 'page' variable 
-    page = Nokogiri::HTML(open("https://www.beeradvocate.com/place/city/1/"))
-    
+  def page
+    #Returns the HTML from the website
+    Nokogiri::HTML(open("https://www.beeradvocate.com/place/city/1/"))
+  end 
+
+  def breweries 
     # Selects the text we want and returns an array of arrays, formatted [["Brewery Name", "Brewery Address"],["Brewery Name", "Brewery Address"],..]
+    # Currently returns breweries and bars/eateries, just want breweries though, so will need to find a way to access just the first "ul".
     page.css("div#ba-content ul li").map {|brewery| brewery.text}.map {|info| info.split(" - ")}
   end 
+  
+  # def brews 
+  #   #Need a way to scrape the individual beers, whose links are on the original page listing breweries.
+  #   breweries.each do {|brewery| brewery}
+  # end
   
 end 
     
