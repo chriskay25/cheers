093aefeba03a8fc05356f2f43bed2619254bd3b6
chriskay <chriskay25@gmail.com>
2019-06-20 12:11:24 -0400

    Deleted the page method from scraper class and made the home page a local variable inside breweries method, significantly sped up brewery instantiation and return. Started moving input loop into make_breweries method in CLI.

--- lib/cheers/cli.rb
+++ lib/cheers/cli.rb
@@ -11,20 +11,22 @@ class Cheers::CLI
     puts "Welcome. This CLI includes information about breweries in Downtown Atlanta."
   end 
     
-    # input = nil 
-    # while input != "exit" 
-    #   input = gets.strip.downcase 
-    #     puts "That is not a valid option, please try again"
 
   def make_breweries
     #Scrapes the breweries page and creates brewery objects.
     Cheers::Scraper.breweries.each do |brewery_hash|
       Cheers::Brewery.new(brewery_hash) 
     end 
+
     puts "Enter the corresponding number of the brewery to learn more:"
     Cheers::Brewery.all.each.with_index(1) do |brewery, index|
       puts "#{index}. #{brewery.name} - #{brewery.address}"
-    end 
+    end
+    
+    # input = nil 
+    # while input != "exit"
+    #   input = gets.strip.downcase 
+    # end 
   end 
   
 end 
--- lib/cheers/scraper.rb
+++ lib/cheers/scraper.rb
@@ -1,24 +1,19 @@
 class Cheers::Scraper
 
   @@brewery_list = []
-  
-  def self.page
-    #Returns the HTML from the website
-    Nokogiri::HTML(open("https://www.beeradvocate.com/place/city/1/"))
-  end 
-
 
   def self.breweries 
     # Uses the page method to get HTML, grabs content from first ul, then selects the text from the li's and split it to return an
     # array of arrays, formatted [["Brewery Name", "Brewery Address"], ["Brewery Name", "Brewery Address"]]
-    page.css("div#ba-content ul").first.css("li").map {|li| li.text}.map {|brewery| brewery.split("  - ")}.each.with_index(1) do |brewery, index|
+    home_pg = Nokogiri::HTML(open("https://www.beeradvocate.com/place/city/1/"))
+    home_pg.css("div#ba-content ul").first.css("li").map {|li| li.text}.map {|brewery| brewery.split("  - ")}.each.with_index(1) do |brewery, index|
       name = brewery[0]
       address = brewery[1] 
       brewery = Hash.new
       brewery[:name] = name 
       brewery[:address] = address 
       brewery[:index] = index 
-      brewery[:url] = "https://www.beeradvocate.com" + page.css("div#ba-content ul").first.css("li a").attribute("href").value
+      brewery[:url] = "https://www.beeradvocate.com" + home_pg.css("div#ba-content ul").first.css("li a").attribute("href").value
       @@brewery_list << brewery
     end
     # @@brewery_list is returned as an array of hashes [{name: name, address: address, url: url}, {name: na...}]
@@ -27,7 +22,6 @@ class Cheers::Scraper
 
 
   def self.brewery_page 
-    @@brewery_hash = {}
     @@brewery_list.map do |brewery| 
       bp = Nokogiri::HTML(open(brewery[:url]))
       beer_count = bp.css("div#stats_box dd").children[0].text
