16ccc977df1ca44c80643cd7f399a31c535bfbd2
chriskay <chriskay25@gmail.com>
2019-06-27 12:22:56 -0400

    Made a number of cosmetic edits to the output in different methods of the cli class, as well as erased some notes. Noticed the loops for invalid inputs were breaking in some spots in brewery_choice and farewell methods. Put a couple of until loops in to try to solve problem. Condensed an iterator in make_beer method to clean up look of code.

--- lib/cheers/cli.rb
+++ lib/cheers/cli.rb
@@ -1,5 +1,3 @@
-# CLI Controller - responsible for user interactions (ex. welcoming user, dealing with inputs, etc.)
-
 class Cheers::CLI
   
   def call 
@@ -9,47 +7,47 @@ class Cheers::CLI
   end 
   
   def greeting 
+    puts ""
     puts "Welcome! This CLI includes information about breweries in Downtown Atlanta.".upcase.yellow
   end 
 
   def make_breweries
-    #Scrapes the breweries page and creates brewery objects.
     Cheers::Scraper.breweries.each do |brewery_hash|
       Cheers::Brewery.new(brewery_hash) 
     end 
   end 
 
   def brewery_choice
-    puts "  Enter the corresponding number of the brewery to see their beer list:".light_white
+    puts ""
+    puts " Enter the corresponding number of a brewery to see their beer list:".light_white
+    puts "------------------------------------------------------------------------".light_white
     Cheers::Brewery.all.each.with_index(1) do |brewery, index|
       puts "#{index.to_s.light_white}. #{brewery.name.upcase.light_green} --- #{brewery.address.cyan}"
     end
     
-    input = nil 
-    while input != "exit"
+    input = gets.to_i
+    until input > 0 && input < Cheers::Brewery.all.count
+      puts "Please enter a valid number:".light_red
       input = gets.to_i
-      if input < 1 || input > Cheers::Brewery.all.count
-        puts "Sorry, that's not a valid input, please enter a valid brewery number:".light_red
-      end 
-      Cheers::Brewery.all.each do |brewery|
-        if input == brewery.index && brewery.beer.empty?
-          make_beer(brewery) 
-        elsif input == brewery.index && brewery.beer.count > 0
-          beer_list(brewery)
-        end 
+    end 
+    
+    Cheers::Brewery.all.each do |brewery|
+      if input == brewery.index && brewery.beer.empty?
+        make_beer(brewery) 
+      elsif input == brewery.index && brewery.beer.count > 0
+        beer_list(brewery)
       end 
     end 
   end 
   
   def make_beer(brewery)
-    Cheers::Scraper.brewery_page(brewery).each do |beer_hash|
-      Cheers::Beer.new(beer_hash)
-    end 
+    Cheers::Scraper.brewery_page(brewery).each {|beer_hash| Cheers::Beer.new(beer_hash)}
     brewery.assign_beer 
     beer_list(brewery)
   end 
 
   def beer_list(brewery)
+    puts ""
     puts "       ------ #{brewery.name.upcase} BEER LIST ------".light_yellow
     brewery.beer.each.with_index(1) do |beer, index|
       puts "#{index}. ".light_white + "#{beer.name.upcase.light_green} " + "| --- | ".light_blue + "STYLE: ".light_white + "#{beer.style.light_cyan}" + " | --- | ".light_blue + "ABV: ".light_white + "#{beer.abv.to_s.light_cyan}" + "%".light_cyan
@@ -60,17 +58,19 @@ class Cheers::CLI
   end  
 
   def farewell 
-    puts "Would you like to see the list of breweries again? (y/n)".yellow
-    puts "--------------------------------------------------------"
-    input = gets.chomp
+    input = nil
     until input == "y" || input == "n"
-      puts "That is not a valid option, would you like to see the list of breweries again? (y/n)".light_red
+      puts "Would you like to see the list of breweries again? (y/n)".light_white
+      input = gets.strip.downcase
     end 
+
     if input == "y"  
       brewery_choice 
     elsif input == "n"
-      puts "Thank you for using this CLI, I hope you found some tasty options!".light_blue 
-      puts "------------------------------------------------------------------".light_white
+      puts "-------------------------------------------------------"
+      puts "Thanks, I hope you found some tasty options!".light_yellow 
+      puts "-------------------------------------------------------".light_white
+      exit
     end  
   end 
 
