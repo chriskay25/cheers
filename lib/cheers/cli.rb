1354c93d78d64c2cc709c2b6cdbf50dc3b197c70
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-01-10 14:46:16 -0500

    Fix issue with alcohol by volume incorporating text from other element

--- lib/cheers/cli.rb
+++ lib/cheers/cli.rb
@@ -1,20 +1,20 @@
 class Cheers::CLI
-  
-  def call 
+
+  def call
     greeting
     make_breweries
-    brewery_choice 
-  end 
-  
-  def greeting 
+    brewery_choice
+  end
+
+  def greeting
     puts "\n Welcome! This CLI includes information about breweries in Downtown Atlanta.".upcase.yellow
-  end 
+  end
 
   def make_breweries
     Cheers::Scraper.breweries.each do |brewery_hash|
-      Cheers::Brewery.new(brewery_hash) 
-    end 
-  end 
+      Cheers::Brewery.new(brewery_hash)
+    end
+  end
 
   def brewery_choice
     puts "\n Enter the corresponding number of a brewery to see their beer list:".light_white
@@ -22,27 +22,28 @@ class Cheers::CLI
     Cheers::Brewery.all.each.with_index(1) do |brewery, index|
       puts "#{index.to_s.light_white}. #{brewery.name.upcase.light_green} --- #{brewery.address.cyan}"
     end
-    
+
+    # Check for valid user input
     input = gets.to_i
     until input > 0 && input <= Cheers::Brewery.all.count
       puts "Please enter a valid number:".light_red
       input = gets.to_i
-    end 
-    
+    end
+
     Cheers::Brewery.all.each do |brewery|
       if input == brewery.index && brewery.beer.empty?
-        make_beer(brewery) 
+        make_beer(brewery)
       elsif input == brewery.index && brewery.beer.count > 0
         beer_list(brewery)
-      end 
-    end 
-  end 
-  
+      end
+    end
+  end
+
   def make_beer(brewery)
     Cheers::Scraper.brewery_page(brewery).each {|beer_hash| Cheers::Beer.new(beer_hash)}
-    brewery.assign_beer 
+    brewery.assign_beer
     beer_list(brewery)
-  end 
+  end
 
   def beer_list(brewery)
     puts "\n     ------ #{brewery.name.upcase} BEER LIST -- #{brewery.beer.count} BREWS AVAILABLE ------".light_yellow
@@ -50,25 +51,25 @@ class Cheers::CLI
       puts "#{index}. ".light_white + "#{beer.name.upcase.light_green} " + "| --- | ".light_blue + "STYLE: ".light_white + "#{beer.style.light_cyan}" + " | --- | ".light_blue + "ABV: ".light_white + "#{beer.abv.to_s.light_cyan}%".light_cyan + " | --- | ".light_blue + "RATING: ".light_white + "#{beer.rating.to_f}/5".light_cyan
       (beer.name.length + 4).times {print "-".light_yellow}
       puts
-    end 
+    end
     farewell
-  end  
+  end
 
-  def farewell 
+  def farewell
     input = nil
     until input == "y" || input == "n"
       puts "Would you like to see the list of breweries again? (y/n)".light_white
       input = gets.strip.downcase
-    end 
+    end
 
-    if input == "y"  
-      brewery_choice 
+    if input == "y"
+      brewery_choice
     elsif input == "n"
       puts "-------------------------------------------------------"
-      puts "Thanks, I hope you found some tasty options!".light_yellow 
+      puts "Thanks, I hope you found some tasty options!".light_yellow
       puts "-------------------------------------------------------".light_white
       exit
-    end  
-  end 
+    end
+  end
 
-end  
+end
--- lib/cheers/scraper.rb
+++ lib/cheers/scraper.rb
@@ -2,35 +2,32 @@ class Cheers::Scraper
 
   @@brewery_list = []
 
-  def self.breweries 
+  def self.breweries
     # Uses the page method to get HTML, grabs content from first ul, then selects the text from the li's and split it to return an
     # array of arrays, formatted [["Brewery Name", "Brewery Address"], ["Brewery Name", "Brewery Address"]]
     home_pg = Nokogiri::HTML(open("https://www.beeradvocate.com/place/city/1/"))
     brewery_urls = home_pg.css("div#ba-content ul").first.css("li a").map {|a| a.attribute("href").value}
     home_pg.css("div#ba-content ul").first.css("li").map {|li| li.text}.map {|brewery| brewery.split("  - ")}.each.with_index(1) do |brewery, index|
       name = brewery[0]
-      address = brewery[1] 
+      address = brewery[1]
       brewery = Hash.new
-      brewery = {name: name, address: address, index: index, url: ("https://www.beeradvocate.com" + brewery_urls[index - 1])} 
+      brewery = {name: name, address: address, index: index, url: ("https://www.beeradvocate.com" + brewery_urls[index - 1])}
       @@brewery_list << brewery
     end
     @@brewery_list
-  end 
+  end
 
 
-  def self.brewery_page(brewery) 
+  def self.brewery_page(brewery)
       bp = Nokogiri::HTML(open(brewery.url))
       bp.css("tbody tr").map do |tr|
-        name = tr.css("td a b").text 
+        name = tr.css("td a b").text
         style = tr.css("td a")[1].text
         rating = tr.css("td b").last.text
-        tr.css("td span").text == "?" ? abv = tr.css("td span").text : abv = tr.css("td span").text.to_f 
-        tr = Hash.new 
+        tr.css("td span")[0].text == "?" ? abv = tr.css("td span")[0].text : abv = tr.css("td span")[0].text.to_f
+        tr = Hash.new
         tr = {brewery: brewery, name: name, style: style, rating: rating, abv: abv}
-      end 
-  end 
+      end
+  end
 
-end 
-
-
-    
+end
